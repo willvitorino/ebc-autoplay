f2458a244f67530ca91802cf69f9e1acbfd49533
William Vitorino <williamvitorino3@gmail.com>
2020-04-04 19:15:33 -0300

    Rebase.

--- src/index.js
+++ src/index.js
@@ -9,15 +9,23 @@ if (require('electron-squirrel-startup')) { // eslint-disable-line global-requir
 const createWindow = () => {
   // Create the browser window.
   const mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
+    width: 500,
+    height: 200,
+    frame: false,
+    webPreferences: {
+      nodeIntegration: false,
+      contextIsolation: true,
+      javascript: true,
+      autoplayPolicy: 'no-user-gerture-required',
+    },
   });
 
   // and load the index.html of the app.
+  mainWindow.setMenuBarVisibility(false);
   mainWindow.loadFile(path.join(__dirname, 'index.html'));
 
   // Open the DevTools.
-  mainWindow.webContents.openDevTools();
+  // mainWindow.webContents.openDevTools();
 };
 
 // This method will be called when Electron has finished
---
+++ src/player.js
@@ -0,0 +1,10 @@
+const player = videojs('transmissao')
+player.play();
+
+function pause() {
+  player.pause()
+}
+
+function play() {
+  player.play()
+}
